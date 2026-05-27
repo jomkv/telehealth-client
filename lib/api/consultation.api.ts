10752500410fdf01042792f13d3f80b9fd7741c2
Jom Karlo Verzosa <jomkarlov@gmail.com>
2026-05-28 01:04:22 +0800

    chore: integrate endpoints for consultations

--- lib/api/consultation.api.ts
+++ lib/api/consultation.api.ts
@@ -3,11 +3,40 @@ import {
   CreateConsultationInput,
 } from "@/@types/consultation";
 import { apiInstance } from "./api";
-import { AvailabilityRow } from "@/@types/availability";
 
 export const consultationApi = {
   async createConsultation(formData: CreateConsultationInput) {
     await apiInstance.post(`/consultation`, formData);
+  },
+
+  async cancelConsultation(consultationId: string) {
+    await apiInstance.patch(`/consultation/${consultationId}/cancel`);
+  },
+
+  async getConsultation(consultationId: string): Promise<ConsultationView> {
+    return (
+      await apiInstance.get<{ data: ConsultationView }>(
+        `/consultation/${consultationId}`,
+      )
+    ).data.data;
+  },
+
+  async saveConsultationNotes(consultationId: string, doctorNotes: string) {
+    return await apiInstance.patch(
+      `/consultation/${consultationId}/doctor-notes`,
+      {
+        doctorNotes,
+      },
+    );
+  },
+
+  async reschedConsultation(consultationId: string, newSchedule: string) {
+    return await apiInstance.patch(
+      `/consultation/${consultationId}/reschedule`,
+      {
+        scheduledAt: newSchedule,
+      },
+    );
   },
 
   async getDoctorConsultations(): Promise<ConsultationView[]> {
